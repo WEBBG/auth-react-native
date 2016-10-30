da08ca9215bcb81571944143cf76ed3f41c79399
Bruno Garcia <bruno@webcomum.com>
2016-10-30 01:38:25 +0100

    login form and connection to firebase

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
-import { View , Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
+
 
 class App extends Component {
+  componentWillMount(){
+    // Initialize Firebase
+    firebase.initializeApp({
+      apiKey: 'AIzaSyB27IBkV7ijTMu9UULLfV1E3Xz6Dy1o-SQ',
+      authDomain: 'auth-4bc97.firebaseapp.com',
+      databaseURL: 'https://auth-4bc97.firebaseio.com',
+      storageBucket: 'auth-4bc97.appspot.com',
+      messagingSenderId: '410779656731'
+    });
+  }
+
   render() {
     return (
       <View>
-        <Text>An App!</Text>
+        <Header headerText="Authentication" />
+        <LoginForm />
       </View>
     );
   }
